2298f140cfa60f3659190a6ad304de833a22297e
annkrs <karas-anna@outlook.com>
2018-02-08 03:26:25 +0100

    little code cleanup, introduction rules

--- checking.ml
+++ checking.ml
@@ -1,21 +1,97 @@
 open List
 open Common
 
-let checkProof (axioms:component list) ((identifier, goal, proof):string * component * component list) =
+
+(* AUXILIARY FUNCTIONS *)
+
+let isFrame (comp:component) = 
+	(* checks if component is a Frame *)
+	match comp with
+		| Frame (_, _) -> true
+		| _ -> false 
+
+let allFrames (env:component list) =
+	(* returns list of all frames from environment *) 
+	List.filter isFrame env
+
+let framesEndingWithComponent (comp:component) (env:component list) = 
+	(* returns a list of all Frame from environment ending with given component *)
+	List.filter (fun x -> 
+		match x with
+		| Frame (a, fp) -> (List.hd (List.rev fp)) = comp
+		| _ -> failwith "tried to find last element of frame proof; frame expected") 
+		(allFrames env)
+
+let existsFrame (assum:component) (last:component) (env:component list) =
+	(* checks if exists a frame with assumption = assum ending with component = last in environment env *)
+	List.exists (fun x -> 
+		match x with
+		| Frame (a, fp) -> a = assum
+		| _ -> failwith "tried to search frames; frame expected") 
+		(framesEndingWithComponent last env)
+
+let isImp (comp:component) = 
+	(* checks if component is an Imp *)
+	match comp with
+		| Imp (_, _) -> true
+		| _ -> false 
+
+let allImps (env:component list) =
+	(* returns list of all Imp from environment *) 
+	List.filter isImp env
+
+
+(* DEDUCING *)
+
+let rec isDerivable (axioms:component list) (expr:component) (env:component list) = 
+	(* tries to derive by checking if exists similar axiom or expression is achievable using inference rules *)
 
 	let existsSimilarAxiom (axioms:component list) (expr:component) = 
 		(* checks if in axioms (list of axioms read so far from input file) exists expression similar ("isomorphic") to given expr*)
 		false in
 
 	let isAchievable (expr:component) (env:component list) = 
-		(* checks if is it possible to deduce expression from environment using inference rules *)
-		true in
+		(* checks whether is it possible to deduce expression from environment using inference rules *)
+		
+		let wasIntroduced = 
+			(* checks possibility to introduce connective from environment *)
+			match expr with 
+			| True -> true 
+			| Neg (Neg (x)) -> 
+				isDerivable axioms x env
+			| Neg (x) -> 
+				existsFrame x False env || (* modus tollens below *)
+					List.exists (fun x -> 
+						match x with 
+						| Imp (a, b) -> List.mem (Neg (b)) env
+						| _ -> failwith "tried to check modus tollens; received component is not an Imp") 
+					(allImps env)
+			| Con (x, y) -> 
+				isDerivable axioms x env && isDerivable axioms y env
+			| Dis (x, y) -> 
+				isDerivable axioms x env || isDerivable axioms y env
+			| Bic (x, y) -> 
+				isDerivable axioms (Imp (x, y)) env && isDerivable axioms (Imp (y, x)) env
+			| Imp (x, y) -> 
+				existsFrame x y env 
+			| _ -> failwith "tried to check if component was introduced; received component is a frame" in 
 
-	let isDerivable (axioms:component list) (expr:component) (env:component list) = 
-		(* tries to derive by checking if exists similar axiom or expression is achievable using inference rules *)
-		(existsSimilarAxiom axioms expr) || (isAchievable expr env) in
+		let isResultOfElimination = 
+			(* checks if expression is obtained from connective elimination *)
+			false in
+
+		wasIntroduced || isResultOfElimination in 
+
+	(List.mem expr env) || (existsSimilarAxiom axioms expr) || (isAchievable expr env)
+
+
+(* CHECKING PROOF *)
+
+let checkProof (axioms:component list) ((identifier, goal, proof):string * component * component list) =
+	(* checks whether proof is correct and prints answer *)
 
 	let rec check (proof:component list) (env:component list) =
+		(* traverses proof - list of components *)
 		match proof with 
 		| [] -> (true, env)
 		| (Frame (assum, frameProof)) :: components ->  
@@ -29,10 +105,9 @@ let checkProof (axioms:component list) ((identifier, goal, proof):string * compo
 			else 
 				(false, []) in
 
-	let result = fst (check proof [True]) in 
-
-	if result 
-		then 
-		 (print_string (identifier ^ " is valid\n"); printProof (goal); true)
+	match (check proof [True]) with 
+	| (res, env) ->  
+		if res &&  List.mem goal env	
+			then (print_string (identifier ^ " is correct\n"); printProof (goal); true)
 		else
-		(print_string (identifier ^ " is not valid\n"); false)
+			(print_string (identifier ^ " is not correct\n"); printProof (goal); false)
